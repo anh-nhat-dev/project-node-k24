81d5c73484f0cf49bfe3e09dccbb8c4085054b5b
Cao Anh Nhất <anhnhata8cool@gmail.com>
2020-10-21 17:52:38 +0700

    add product

--- src/apps/controllers/product.js
+++ src/apps/controllers/product.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 const paginate = require("../../common/paginate");
 
 const ProductModel = mongoose.model("Product");
+const CategoryModel = mongoose.model("Category");
 
 module.exports.index = async function (req, res) {
   const page = parseInt(req.query.page) || 1;
@@ -23,4 +26,40 @@ module.exports.index = async function (req, res) {
     page,
     pages: paginate(page, totalPage),
   });
+};
+
+module.exports.create = async (req, res) => {
+  const categories = await CategoryModel.find();
+  res.render("admin/products/add-product", { categories });
+};
+
+module.exports.store = async (req, res) => {
+  const body = req.body;
+  const file = req.file;
+
+  const product = {
+    prd_name: body.prd_name,
+    prd_price: body.prd_price,
+    prd_warranty: body.prd_warranty,
+    prd_accessories: body.prd_accessories,
+    prd_new: body.prd_new,
+    prd_promotion: body.prd_promotion,
+    prd_status: body.prd_status,
+    prd_featured: body.prd_featured,
+    prd_details: body.prd_details,
+    cat_id: body.cat_id,
+  };
+
+  if (file) {
+    fs.renameSync(
+      file.path,
+      path.resolve("src/public/images/products", file.originalname)
+    );
+
+    product["prd_image"] = file.originalname;
+  }
+
+  await new ProductModel(product).save();
+
+  return res.redirect("/admin/products");
 };
--- src/routers/web.js
+++ src/routers/web.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
-const app = require("../app");
+const multer = require("multer");
+const path = require("path");
 const AdminController = require("../apps/controllers/admin");
 const AuthController = require("../apps/controllers/auth");
 const ProductController = require("../apps/controllers/product");
 const checkGuest = require("../apps/middlewares/check-guest");
 const checkLogin = require("../apps/middlewares/check-login");
 const checkLevel = require("../apps/middlewares/check-level");
+const upload = multer({ dest: path.resolve("temp") });
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -15,6 +17,11 @@ router.get("/", (req, res) => {
 router.use("/admin", checkGuest, checkLevel);
 router.get("/admin", AdminController.dashboard);
 router.get("/admin/products", ProductController.index);
+router
+  .route("/admin/products/create")
+  .get(ProductController.create)
+  .post(upload.single("prd_image"), ProductController.store);
+
 router
   .route("/login")
   .all(checkLogin)
